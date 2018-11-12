d14e263dde532fbe988f77ab68f7cfd9ff0f9599
Arkhanne <eduardbenito@icloud.com>
2018-11-12 14:32:33 +0100

    Mixins

--- movie.rb
+++ movie.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require_relative 'rankable'
+
 #
 # Movie
 #
 class Movie
-  attr_reader :rank
+  include Rankable
+
+  attr_accessor :rank
   attr_accessor :title
 
   def initialize(title, rank=0)
@@ -15,7 +19,7 @@ class Movie
 
   def self.from_csv(line)
     title, rank = line.split(',')
-    movie = Movie.new(title, Integer(rank))
+    Movie.new(title, Integer(rank))
   end
 
   def to_csv
@@ -37,30 +41,6 @@ class Movie
     @snack_carbs[snack.name] += snack.carbs
     puts "#{@title} led to #{snack.carbs} #{snack.name} carbs being consumed."
     puts "#{@title}'s snacks: #{@snack_carbs}"
-  end
-
-  def hit?
-    @rank >= 10
-  end
-
-  def status
-    hit? ? 'Hit' : 'Flop'
-  end
-
-  def normalized_rank
-    @rank / 10
-  end
-
-  def thumbs_up
-    @rank += 1
-  end
-
-  def thumbs_down
-    @rank -= 1
-  end
-
-  def <=>(other)
-    other.rank <=> @rank
   end
 
   def to_s
