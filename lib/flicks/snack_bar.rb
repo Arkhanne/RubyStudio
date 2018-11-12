f452f4ca1052a9d143ea6aae10f38a530ebd1303
Arkhanne <eduardbenito@icloud.com>
2018-11-12 16:42:08 +0100

    Distribution

--- lib/flicks/snack_bar.rb
+++ lib/flicks/snack_bar.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Snack = Struct.new(:name, :carbs)
 
+#
+# SnackBar
+#
 module SnackBar
   SNACKS = [
     Snack.new(:popcorn, 20),
@@ -7,14 +12,14 @@ module SnackBar
     Snack.new(:nachos, 40),
     Snack.new(:pretzel, 10),
     Snack.new(:soda, 5)
-  ]
+  ].freeze
 
   def self.random
     SNACKS.sample
   end
 end
 
-if __FILE__ == $0
+if $PROGRAM_NAME == __FILE__
   puts SnackBar::SNACKS
   snack = SnackBar.random
   puts "Enjoy your #{snack.name} (#{snack.carbs} carbs)"
